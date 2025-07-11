440c294c43bd718ee7f76496f790e6101999de3a
Ulisse <ulissebuccella@gmail.com>
2025-07-11 20:20:24 +0200

    Initial commit - CRUD Ulisse project

---
+++ src/main/java/com/ulisse/crud_ulisse/CrudUlisseApplication.java
@@ -0,0 +1,13 @@
+package com.ulisse.crud_ulisse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudUlisseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudUlisseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ulisse/crud_ulisse/CrudUlisseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ulisse.crud_ulisse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudUlisseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
